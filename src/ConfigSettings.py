9d9ecbb61d935d876d7be64e69719223a3074f0c
Nodever2 <nddevern@mtu.edu>
2023-10-04 21:34:53 -0400

    Comment formatting in ConfigSettings

--- src/ConfigSettings.py
+++ src/ConfigSettings.py
@@ -1,13 +1,20 @@
 # Configuration Settings
+# None of this data is checked/validated in the code so make sure it makes sense.
 
-NUM_COLUMNS = 3 # The number of vertical columns to display the undrafted pokemon list in.
+# The number of vertical columns to display the undrafted pokemon list in.
+NUM_COLUMNS = 3
 
-DISPLAY_UNDRAFTED_POKEMON_TYPES = True # Display the types of pokemon in the undrafted pokemon list.
+# Display the types of pokemon in the undrafted pokemon list.
+DISPLAY_UNDRAFTED_POKEMON_TYPES = True
 
-DISPLAY_UNDRAFTED_POKEMON_EVOLUTION_INFO = True # Display the maximum potential evolution of undrafted pokemon and whether they can mega (This does not affect sorting)
+# Display the maximum potential evolution of undrafted pokemon and whether they can mega (This does not affect sorting)
+DISPLAY_UNDRAFTED_POKEMON_EVOLUTION_INFO = True
 
-DEFAULT_COMMAND = "/draft" # If this is set to "", you will be prompted to enter a command when the script starts. The code does not validate this so make sure you enter a valid command here.
+# If this is set to "", you will be prompted to enter a command when the script starts.
+DEFAULT_COMMAND = "/draft"
 
-DEFAULT_POKEMON_TO_GENERATE_PER_PLAYER = 12 # Set this to -1 to have the script ask you how many pokemon you'd like to randomly generate.
+# Set this to -1 to have the script ask you how many pokemon you'd like to randomly generate.
+DEFAULT_POKEMON_TO_GENERATE_PER_PLAYER = 12
 
-CONFIRM_POKEMON_PICKS = True # Controls whether the script will ask you to confirm every time you choose a pokemon.
+# Controls whether the script will ask you to confirm every time you choose a pokemon.
+CONFIRM_POKEMON_PICKS = True
